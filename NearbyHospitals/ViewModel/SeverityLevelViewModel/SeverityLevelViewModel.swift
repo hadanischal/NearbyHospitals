0bd84219bd24455d6febfa763a87767fe077b3df
Nischal Hada <hadanischal@gmail.com>
2020-06-28 11:14:13 +1000

    [Viewmodel] Implemented SeverityLevelViewModel

---
+++ NearbyHospitals/ViewModel/SeverityLevelViewModel/SeverityLevelViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  SeverityLevelViewModel.swift
+//  NearbyHospitals
+//
+//  Created by Nischal Hada on 21/6/20.
+//  Copyright © 2020 Nischal Hada. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol SeverityLevelDataSource: AnyObject {
+    var navigationTitle: Observable<String> { get }
+    var numbersSeverity: Int { get }
+    var collectionViewHeader: String { get }
+    func severity(forIndex index: Int) -> LevelOfPain
+}
+
+final class SeverityLevelViewModel: SeverityLevelDataSource {
+    private var illnessesList: [LevelOfPain] { LevelOfPain.allCases }
+
+    var navigationTitle: Observable<String> { Observable.just(L10n.SeverityLevel.navigationTitle) }
+    var collectionViewHeader: String { L10n.SeverityLevel.collectionviewViewTitle }
+    var numbersSeverity: Int { self.illnessesList.count }
+
+    func severity(forIndex index: Int) -> LevelOfPain {
+        self.illnessesList[index]
+    }
+}
